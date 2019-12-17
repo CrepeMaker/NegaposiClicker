a2f46248bec655a89202221babdba1a178045daa
Yoshida Takuto <takuto.yoshida@gmail.com>
2019-12-17 14:36:54 +0900

    modify deploy.sh

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
 
+set -eux
+
 git config git-ftp.url ftpes://sv38.star.ne.jp/negaposi.crepemaker.xyz
 git config git-ftp.user ${FTP_USER}
 git config git-ftp.password ${FTP_PASSWORD}
 git config git-ftp.syncroot public
 
 git ftp catchup
-git ftp push
+git ftp push -a
