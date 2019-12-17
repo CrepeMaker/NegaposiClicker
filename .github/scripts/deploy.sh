05764121a186ea651b88923b397d9a5a4c9b0cf9
Yoshida Takuto <takuto.yoshida@gmail.com>
2019-12-17 14:33:38 +0900

    create deploy.sh

---
+++ .github/scripts/deploy.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+git config git-ftp.url ftpes://sv38.star.ne.jp/negaposi.crepemaker.xyz
+git config git-ftp.user ${FTP_USER}
+git config git-ftp.password ${FTP_PASSWORD}
+git config git-ftp.syncroot public
+
+git ftp catchup
+git ftp push
